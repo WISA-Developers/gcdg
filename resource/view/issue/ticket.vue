4eeec8341fce8fc9c4672cbe4eb51dea57738a80
zardsama <zardsama@naver.com>
2023-12-12 17:19:06 +0900

    티켓 등록 시 수정 권한 "등록자와 담당자"가 기본으로 되도록 변경

--- resource/view/issue/ticket.vue
+++ resource/view/issue/ticket.vue
@@ -106,8 +106,8 @@
 					<th>수정권한</th>
 					<td>
 						<select name="permission" class="input_select">
-							<option value="1">등록자만 수정</option>
 							<option value="2">등록자와 담당자</option>
+                            <option value="1">등록자만 수정</option>
 							<option value="0">전체</option>
 						</select>
 					</td>
